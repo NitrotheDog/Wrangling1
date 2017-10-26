b1337b5eb8e842e1c27fdd6003405561ac675e37
NitrotheDog <sswells@sbcglobal.net>
2017-10-25 23:12:39 -0700

    p2

--- WranglingEX1.R
+++ WranglingEX1.R
@@ -3,6 +3,11 @@ library(dplyr)
 
 refine_df <- tbl_df(refine_original)
 
+# change first column to lowercase
 refine_df$company <- tolower(refine_df$company)
 
-View(refine_df)
+# separate product code from product number
+refine_df <- refine_df %>% 
+  separate(`Product code / number`, c("product_code", "product_number"), sep = "-")
+
+
